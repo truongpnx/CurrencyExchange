71a0ab2b3881f338fb97aa126e9ae543217acb52
Truong Pham <xuantruongpnxt@gmail.com>
2024-10-29 16:37:10 +0700

    Change Double to BigDecimal for more precision

--- app/src/main/java/com/example/currencyexchange/helper/StringHelper.kt
+++ app/src/main/java/com/example/currencyexchange/helper/StringHelper.kt
@@ -1,14 +1,22 @@
 package com.example.currencyexchange.helper
 
+import java.math.BigDecimal
 import java.math.RoundingMode
 import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
 import java.time.Instant
 import java.time.LocalDate
+import java.util.Locale
 
 object StringHelper {
 
-    fun formatCurrency(amount: Double): String {
-        val decimalFormat = DecimalFormat("#,##0.######")
+    fun formatCurrency(amount: BigDecimal): String {
+        val symbols = DecimalFormatSymbols(Locale.US).apply {
+            decimalSeparator = '.' // Set dot as the decimal separator
+            groupingSeparator = ',' // Set comma as the grouping separator
+        }
+
+        val decimalFormat = DecimalFormat("#,##0.######", symbols)
         decimalFormat.roundingMode = RoundingMode.HALF_UP
         return decimalFormat.format(amount)
     }
@@ -21,5 +29,9 @@ object StringHelper {
 
     fun currencyToDouble(currency: String): Double {
         return currency.replace(",", "").toDouble()
+    }
+
+    fun currencyToBigDecimal(currency: String): BigDecimal {
+        return currency.replace(",", "").toBigDecimal()
     }
 }
--- app/src/test/java/com/example/currencyexchange/HelperTest.kt
+++ app/src/test/java/com/example/currencyexchange/HelperTest.kt
@@ -1,5 +1,7 @@
 package com.example.currencyexchange
 
+import com.example.currencyexchange.helper.StringHelper.currencyToDouble
+import com.example.currencyexchange.helper.StringHelper.formatCurrency
 import com.example.currencyexchange.helper.StringHelper.timestampToYYYYMMDD
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -24,5 +26,22 @@ class HelperTest {
         val resultDate = timestampToYYYYMMDD(timestamp)
 
         assertEquals(expectedDate, resultDate)
+    }
+
+    @Test
+    fun testFormatCurrency_CorrectFormat() {
+
+        val amount = 1234567.89
+        val expectedFormattedAmount = "1,234,567.89"
+        val formattedAmount = formatCurrency(amount)
+        assertEquals(expectedFormattedAmount, formattedAmount)
+    }
+
+    @Test
+    fun testCurrencyToDouble_CorrectConversion() {
+        val currency = "1,234,567.89"
+        val expectedAmount = 1234567.89
+        val amount = currencyToDouble(currency)
+        assertEquals(expectedAmount, amount, 0.0)
     }
 }
